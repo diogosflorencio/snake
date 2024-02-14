05b42f9a17ab070b38755c04009b21511513968e
Diogo S. Florêncio <33941005+diogosflorencio@users.noreply.github.com>
2024-02-13 22:52:22 -0300

    trocando cor

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ const tela = document.querySelector(".tela");
 const criaCobrinha = () => {
     const cobrinha = tela.getContext("2d");
     cobrinha.beginPath();
-    cobrinha.fillStyle = 'black';
+    cobrinha.fillStyle = 'white';
     cobrinha.arc(10,10,10,0,4*3.14);
     cobrinha.fill(); 
 }
