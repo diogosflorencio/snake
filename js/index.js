75c7a32d510b1bd93b85f25206cb94ce9395a78c
Diogo S. Florêncio <33941005+diogosflorencio@users.noreply.github.com>
2024-02-14 21:23:39 -0300

    não sei

--- js/index.js
+++ js/index.js
@@ -1,10 +1,31 @@
 const tela = document.querySelector(".tela");
 
-const criaCobrinha = () => {
-    const cobrinha = tela.getContext("2d");
-    cobrinha.beginPath();
-    cobrinha.fillStyle = 'white';
-    cobrinha.arc(10,10,10,0,4*3.14);
-    cobrinha.fill(); 
+// botoes
+const cima = document.querySelector(".cima");
+const direta = document.querySelector(".direita");
+const esquerda = document.querySelector(".esquerda");
+const baixo = document.querySelector(".baixo");
+
+const criaCobrinha = (param) => {
+    let x = 175
+    let y = 175
+    let tamanho = 1
+    setInterval(()=>{
+        for (let index = 0; index <  tamanho; index++) {
+            const cobrinha = tela.getContext("2d");
+            cobrinha.beginPath();
+            cobrinha.fillStyle = 'white'; 
+            cobrinha.arc(x,y-=20,10,0,4*3.14);
+            cobrinha.fill();   
+        }
+    },1000) 
 }
-criaCobrinha();
+const movimentaCobrinha = (param) => {
+    if(param == "cima"){
+        criaCobrinha("cima")
+    }
+}
+cima.addEventListener("click", () => movimentaCobrinha("cima"));
+direta.addEventListener("click", () => movimentaCobrinha("direta"));
+esquerda.addEventListener("click", () => movimentaCobrinha("esquerda"));
+baixo.addEventListener("click", () =>movimentaCobrinha("baixo"));
